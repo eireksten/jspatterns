c874f73c761a10c38fb4e76b1f8335d22dbac5e2
Eirik Reksten <eireksten@gmail.com>
2014-05-25 15:28:29 +0200

    Added jshint configuration.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,27 @@
 module.exports = function(grunt) {
 
-  grunt.registerTask('default', []);
+  grunt.initConfig({
+    jshint: {
+      options: {
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        indent: 4,
+        noempty: true,
+        undef: true,
+        unused: true,
+        strict: true,
+        browser: true,
+        devel: true,
+        predef: ['Zepto', '$', '_'],
+        ignores: ['js/templates.js']
+      },
+      src: ['js/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
 
 };
